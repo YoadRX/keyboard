92d92bece8a0032ce6e7d1383924e2b60044dd70
YoadRX <yoadrimok@hilma.tech>
2023-11-12 11:46:34 +0200

    KEys Delete and Space

--- src/Components/KeyBoard.jsx
+++ src/Components/KeyBoard.jsx
@@ -5,13 +5,17 @@ const KeyBoard = ({ value, lang }) => {
   const [keys, setKeys] = useState(value);
   const values = keys.split("");
   return (
-    <div>
-      {values.map((key, index) => (
-        <button key={index} id={index}>
-          {key}
-        </button>
-      ))}
-    </div>
+    <>
+      <div>
+        {values.map((key, index) => (
+          <button key={index} id={index}>
+            {key}
+          </button>
+        ))}
+        <button id="Space">Space</button>
+        <button id="Delete">Delete</button>
+      </div>
+    </>
   );
 };
 
