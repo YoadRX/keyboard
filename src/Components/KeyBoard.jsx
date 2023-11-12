d0c3c0abd773d2074af9c45b1ddf1c69a2c61771
YoadRX <yoadrimok@hilma.tech>
2023-11-12 11:40:33 +0200

    useState Keyboard

--- src/Components/KeyBoard.jsx
+++ src/Components/KeyBoard.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useState } from "react";
 
 const KeyBoard = ({ value, lang }) => {
-  const keys = value.split("");
+  const [keys, setKeys] = useState(value);
+  const values = keys.split("");
   return (
     <div>
-      {keys.forEach((element) => (
-        <button>{element}</button>
+      {values.map((key, index) => (
+        <button key={index} id={index}>
+          {key}
+        </button>
       ))}
     </div>
   );
