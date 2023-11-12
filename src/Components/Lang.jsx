42e81474e36d1d94e3ede7609365579bb57ea0aa
shoham3 <shohampressler@hilma.tech>
2023-11-12 14:13:30 +0200

    nothing rn

--- src/Components/Lang.jsx
+++ src/Components/Lang.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { useState } from "react";
 
 const Lang = () => {
-  return <div>Lang</div>;
+  return (
+    <div>
+      <button type="button">Lang</button>
+      <div id="languages-container">
+        <button>English</button>
+        <button>Hebrew</button>
+        <button>Spanish</button>
+      </div>
+    </div>
+  );
 };
 
 export default Lang;
